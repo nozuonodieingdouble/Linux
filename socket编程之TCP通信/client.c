384f88ecefb9d9457dd1f684ae5f50e11ba5deb2
nozuonodieingdouble <1532511761@qq.com>
2017-07-11 21:35:19 +0800

    客户端程序

---
+++ socket编程之TCP通信/client.c
@@ -0,0 +1,62 @@
+//client.c
+
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+
+static void usage(const char* proc)
+{
+        printf("Usage %s[server_ip][server_port]\n", proc);
+}
+
+
+int main(int argc, char* argv[]){
+if (argc != 3)
+        {
+                usage(argv[0]);
+                return 1;
+        }
+
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+if (sock < 0)
+        {
+                perror("sock");
+                return 2;
+       }
+
+        struct sockaddr_in remote;
+        remote.sin_family = AF_INET;
+        remote.sin_port = htons(atoi(argv[2]));
+        remote.sin_addr.s_addr = inet_addr(argv[1]);
+
+        if (connect(sock, (struct sockaddr*) &remote, sizeof(remote)) < 0)
+        {
+                perror("connect");
+                return 3;
+        }
+
+        while (1)
+ 	{
+                printf("Please Enter#");
+                fflush(stdout);
+                char buf[1024];
+                ssize_t s = read(0, buf, sizeof(buf)-1);
+                if (s > 0)
+                {
+                        buf[s - 1] = 0;
+                        write(sock, buf, strlen(buf));
+                        ssize_t _s = read(sock, buf, sizeof(buf)-1);
+                        if (_s > 0)
+                        {
+                                buf[_s] = 0;
+                                printf("server # %s\n", buf);
+                        }
+                }
+        }
+}
